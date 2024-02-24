bd6dcfe3f932d3f68c276f6d1ed164b027984321
Sarmad Rehman <sarmadr02@gmail.com>
2024-02-24 20:30:48 +0500

    simplified the payload logic

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -2,8 +2,8 @@ import { useState, useReducer } from "react";
 
 function reducer(state, action) {
   console.log(state, action);
-  if (action.type === "inc") return state + action.payload;
-  if (action.type === "dec") return state - action.payload;
+  if (action.type === "inc") return state + 1;
+  if (action.type === "dec") return state - 1;
   if (action.type === "setCount") return action.payload;
   // return Number(state) + Number(action);
 }
