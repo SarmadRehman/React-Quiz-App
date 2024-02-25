42d79d396101f0e6cf63e6d7d6b5bb30d977f12a
Sarmad Rehman <sarmadr02@gmail.com>
2024-02-25 16:38:12 +0500

    json-server scripts are added in package to mock an API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import DateCounter from "./DateCounter";
+import Header from "./Header";
 import "./index.css";
+import Main from "./Main";
 function App() {
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+      <Main className="main">
+        <p>1/15</p>
+        <p>Questions</p>
+      </Main>
     </div>
   );
 }
