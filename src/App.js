739a67d2b76fa0febf7557ad3f8e7cb3ea75db04
Sarmad Rehman <sarmadr02@gmail.com>
2024-02-25 16:44:55 +0500

    data fetched from json-server

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import Header from "./Header";
 import "./index.css";
 import Main from "./Main";
+import { useEffect } from "react";
 function App() {
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  });
   return (
     <div className="app">
       <Header />
